af7104a5086687ab2182e57b6eb1c7c46ef94f3a
barkinvar <varbarkn@gmail.com>
2022-06-13 23:52:20 +0300

    Turkish Language

--- video_creation/voices.py
+++ video_creation/voices.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from mutagen.mp3 import MP3
 from utils.console import print_step, print_substep
 from rich.progress import track
-
+import subprocess
 
 def save_text_to_mp3(reddit_obj):
     """Saves Text to MP3 files.
@@ -17,8 +17,7 @@ def save_text_to_mp3(reddit_obj):
     # Create a folder for the mp3 files.
     Path("assets/mp3").mkdir(parents=True, exist_ok=True)
 
-    tts = gTTS(text=reddit_obj["thread_title"], lang="en", slow=False)
-    tts.save(f"assets/mp3/title.mp3")
+    subprocess.run(['edge-tts', "--voice", "tr-TR-EmelNeural", '--text', str(reddit_obj["thread_title"]), '--write-media', "assets/mp3/title.mp3"])
     length += MP3(f"assets/mp3/title.mp3").info.length
 
     try:
@@ -27,16 +26,14 @@ def save_text_to_mp3(reddit_obj):
         pass
 
     if reddit_obj["thread_post"] != "":
-        tts = gTTS(text=reddit_obj["thread_post"], lang="en", slow=False)
-        tts.save(f"assets/mp3/posttext.mp3")
+        subprocess.run(['edge-tts', "--voice", "tr-TR-EmelNeural", '--text', str(reddit_obj["thread_post"]), '--write-media', "assets/mp3/posttext.mp3"])
         length += MP3(f"assets/mp3/posttext.mp3").info.length
 
     for idx, comment in track(enumerate(reddit_obj["comments"]), "Saving..."):
         # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
         if length > 50:
             break
-        tts = gTTS(text=comment["comment_body"], lang="en", slow=False)
-        tts.save(f"assets/mp3/{idx}.mp3")
+        subprocess.run(['edge-tts', "--voice", "tr-TR-EmelNeural", '--text', str(comment["comment_body"]), '--write-media', f"assets/mp3/{idx}.mp3"])
         length += MP3(f"assets/mp3/{idx}.mp3").info.length
 
     print_substep("Saved Text to MP3 files successfully.", style="bold green")
